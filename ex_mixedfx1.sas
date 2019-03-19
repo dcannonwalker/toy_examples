52fe34c4bcbe62e25cd4b03c61589c3407931718
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2019-03-19 18:14:59 -0500

    Fix construction of df
    used product of Z matrix and random xu effect in data construction, added column for xu in df

--- ex_mixedfx1.sas
+++ ex_mixedfx1.sas
@@ -8,13 +8,33 @@ value x
      3 = "3" 
 ;
 
+value xu 
+     1 = "1" 
+     2 = "2" 
+     3 = "3" 
+     4 = "4" 
+     5 = "5" 
+     6 = "6" 
+     7 = "7" 
+     8 = "8" 
+     9 = "9" 
+     10 = "10" 
+     11 = "11" 
+     12 = "12" 
+     13 = "13" 
+     14 = "14" 
+     15 = "15" 
+;
+
 DATA  rdata ;
 INFILE  "U:/ex_mixedfx1.txt" 
      DSD 
-     LRECL= 12 ;
+     LRECL= 15 ;
 INPUT
  x
+ xu
  y
 ;
 FORMAT x x. ;
+FORMAT xu xu. ;
 RUN;
--- ex_mixedfx1_data_generation.R
+++ ex_mixedfx1_data_generation.R
@@ -2,14 +2,17 @@
 #analysis in SAS.
 
 x <- factor(rep(c(1,2,3), each = 10))
+xu <- factor(rep(1:15, each = 2))
 beta <- c(2.5, 1.5,4.5)
 x
+xu
 
-u <- rnorm(length(x),sd = 0.3)
+u <- rnorm(15,sd = 0.3)
 e <- rnorm(length(x),sd = 1.1)
-y <- round(drop(model.matrix(~0+x)%*%beta) + u + e, 4)
+y <- round(drop(model.matrix(~0+x)%*%beta) + 
+             drop(model.matrix(~0+xu))%*%u + e, 4)
 
-df <- data.frame(x,y)
+df <- data.frame(x,xu,y)
 
 library(foreign)
 write.foreign(df, "U:/ex_mixedfx1.txt", "U:/ex_mixedfx1.sas",
